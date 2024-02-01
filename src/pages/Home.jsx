99d2c9a581fa46418fb9b1750257f86535352343
suraj <paudel.suraj76@gmail.com>
2024-02-01 11:15:45 +0000

    feature: setup project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <>
+      <div className="container">
+        <header className="header">
+          <div>P</div>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default Home;
